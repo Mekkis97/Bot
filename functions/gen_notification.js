2ef3ca2ab5e99dd3eb8067f23e3ebededc762408
Mekkis97 <antonius.mekhael@hotmail.com>
2023-08-03 02:20:31 +0200

    #FinalAlpha

--- functions/gen_notification.js
+++ functions/gen_notification.js
@@ -2,6 +2,6 @@ const moment = require("moment")
 const { subscription_day, notification_day } = require("../config/config")
 
 module.exports = () => {
-    const notifications_day = subscription_day - notification_day
+    const notifications_day = subscription_day - notification_day 
     return moment().add(notifications_day , 'days')
 }
