3c22c140f82a821eb544987c15da6f7bcb7acc9a
Mekkis97 <antonius.mekhael@hotmail.com>
2023-08-16 22:06:49 +0200

    #11 new

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const bot_token = '6370982273:AAHYaBl2kXdvkPs884OR6_5W42Vx9ujWX9I'
-const db_url = 'mongodb://127.0.0.1:27017/group_payment_bot' //'mongodb://127.0.0.1:27017/channel_payment_bot'
+const db_url = 'mongodb://127.0.0.1:27017/channel_payment_bot' //'mongodb://127.0.0.1:27017/channel_payment_bot'
 // const stripe_key = '350862534:LIVE:ZTJmOGQzNzc1Mjkx'
 const stripe_key = '350862534:LIVE:ZTJmOGQzNzc1Mjkx'
 const telegram_group = 'https://t.me/neondragon_ai'
